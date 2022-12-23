7d156274dd5b85206d5536a817e6b873adcc94cd
Administrator <Administrator@DESKTOP-PCMCSF2>
2022-12-23 18:23:30 +0530

    [Avani]:class created

---
+++ DummyGithub/src/Demo/Sample3.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class Sample3 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi good Evening ");
+		System.out.println("how was the day");
+		System.out.println("I m tester");
+	}
+}
